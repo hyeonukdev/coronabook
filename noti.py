11c1b11569bfd4150bc7a76810e3e132f08723cd
hyeonukdev <lego7407@gmail.com>
2020-03-16 03:44:24 +0900

    token

--- noti.py
+++ noti.py
@@ -2,13 +2,10 @@ from config import TELEGRAM_TOKEN, CHAT_ID
 import requests as rq
 import telegram
 
-my_token = 'TELEGRAM_TOKEN'
-
-bot = telegram.Bot(token=my_token)
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 updates = bot.getUpdates()
 
 
 def send(t):
     bot.sendMessage(CHAT_ID, t, parse_mode=telegram.ParseMode.HTML)
-   
